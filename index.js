73a101deaef7d54b9813e845c5c1e4dc14612bc5
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-23 22:32:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+function theBeatlesPlay() {
+  
+}
+function johnLennonFacts() {
+  
+}
+function iLoveTheBeatles(array) {
+  do {
+    console.log("I love the Beatles!")
+  } while (array < 15; array++)
+}
