95f47792d965f5c525d22839518a520f007d87d0
kegranados1@gmail.com <kegranados1@gmail.com>
2018-07-23 22:41:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-// add solution here
-function theBeatlesPlay() {
-  
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array;
 }
-function johnLennonFacts() {
-  
+function johnLennonFacts(facts) {
+  var results = [];
+  var count = 0;
+  while (count < facts.length) {
+    results.push(facts[count] + "!!!");
+    count++;
+  }
+  return results;
 }
-function iLoveTheBeatles(array) {
+function iLoveTheBeatles(num) {
+  var array = [];
   do {
-    console.log("I love the Beatles!")
-  } while (array < 15; array++)
+    array.push("I love the Beatles!")
+    num++;
+  } while (num < 15);
+  return array;
 }
